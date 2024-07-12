574dafb40a7a1c534708c111017596f94dfa6533
Raka RG <rakaguntara10@gmail.com>
2024-07-12 09:22:07 +0700

    Show list

--- app/src/main/java/com/rakarguntara/movieapp/MainActivity.kt
+++ app/src/main/java/com/rakarguntara/movieapp/MainActivity.kt
@@ -3,13 +3,37 @@ package com.rakarguntara.movieapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.rakarguntara.movieapp.ui.theme.MovieAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,6 +47,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyApp(content: @Composable () -> Unit){
     MovieAppTheme {
@@ -31,14 +56,78 @@ fun MyApp(content: @Composable () -> Unit){
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-            content()
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        title = { Text("Movie App", style = TextStyle(
+                            color = Color.White
+                        )) },
+                        colors = TopAppBarDefaults.topAppBarColors(
+                            containerColor = Color.Red,
+                        ),
+                    )
+                },
+            ){
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(it)
+                ) {
+                    Text(
+                        text = "List Movie",
+                        modifier = Modifier
+                            .padding(horizontal = 16.dp, vertical = 8.dp)
+                    )
+                    content()
+                }
+            }
+
         }
     }
 }
 
 @Composable
-fun MyContent(){
+fun MyContent(movieList: List<String> = listOf(
+    "Agak Lain", "AsepTheDragon", "AsepPencilgon", "Gundam Seed", "Gundam", "Gundam Seed Destiny", "Kyoukai no Kanata"
+)){
+    Column(modifier = Modifier
+        .padding(horizontal = 16.dp)
+    ){
+        LazyColumn{
+            items(items = movieList){
+                MovieItem(it)
+            }
+        }
+    }
+}
 
+@Composable
+fun MovieItem(title: String){
+    Card(modifier = Modifier
+        .fillMaxWidth()
+        .height(120.dp)
+        .padding(bottom = 8.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        shape = RoundedCornerShape(10.dp)
+    ){
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Surface (Modifier.padding(16.dp)
+                .size(80.dp),
+                shape = RectangleShape,
+                shadowElevation = 4.dp){
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Movie",
+                    tint = Color.Cyan)
+            }
+            Text(text = title, style = TextStyle(
+                color = Color.Black
+            ))
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
